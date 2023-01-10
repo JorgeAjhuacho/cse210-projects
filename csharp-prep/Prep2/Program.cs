b3a670e566e713e125c6fadce3c07a3690c951c1
jorgeajhuacho <ajh21001@byui.edu>
2023-01-09 23:02:29 -0400

    Prep2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        string gradeLetter = "";
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >=70 && grade < 80)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed the course.");
+        }
+        else 
+        {
+            Console.WriteLine("You'll do better the next time.");
+        }
+
     }
 }
