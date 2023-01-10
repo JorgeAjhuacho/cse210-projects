759838f7b72dc8440c86c7eef5742fdb2c63d844
jorgeajhuacho <ajh21001@byui.edu>
2023-01-10 15:53:20 -0400

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int numberRandom = randomGenerator.Next(1, 51);
+        
+        int Number = -1;
+
+        while (Number != numberRandom)
+        {
+            Console.Write("What is your guess? ");
+            Number = int.Parse(Console.ReadLine());
+
+            if (numberRandom > Number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numberRandom < Number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed. Correct!");   
+            }
+        }
+        
     }
 }
