53cbc62d759b3b0a17495e3e980c93af14a914cf
jorgeajhuacho <ajh21001@byui.edu>
2023-01-11 11:07:41 -0400

    Testing print list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>(); //List created
+        int numAdded = -1;  
+        while (numAdded != 0 )  //Until numAdded =0, will continue the loop
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            numAdded = int.Parse(input);
+
+            numbers.Add(numAdded);
+        } 
+
+        //Test to print list
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }     
+        
     }
 }
