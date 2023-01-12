fddb0ae3dfe03fddda94bf10de0ab0e4cbcb83a7
jorgeajhuacho <ajh21001@byui.edu>
2023-01-12 10:20:28 -0400

    Instance 1 created

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Setting instance 1 'job1'
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Console.WriteLine(job1._company);
     }
 }
