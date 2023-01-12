1c3dc25b2c02a57ba807611e289f2cef2d05533f
jorgeajhuacho <ajh21001@byui.edu>
2023-01-12 10:47:33 -0400

    file completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -11,6 +11,19 @@ class Program
         job1._startYear = 2018;
         job1._endYear = 2021;
 
-        Console.WriteLine(job1._company);
+        //Setting instance 2 'job2'
+        Job job2 = new Job();
+        job2._jobTitle = "Network Engineer";
+        job2._company = "Cisco";
+        job2._startYear = 2015;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gerald Johnson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
