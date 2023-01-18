16002e51cec35c4962827c995f48813c369e4e73
jorgeajhuacho <ajh21001@byui.edu>
2023-01-17 22:45:28 -0400

    Questions added

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Questions added to a list
+        List<string> prompts = new List<string>();
+        prompts.Add("What did you learn today and how will it impact your future?");
+        prompts.Add("What is something that you are grateful for today");
+        prompts.Add("How did you handle a difficult situation today?");
+        prompts.Add("What are your thoughts and feelings about your health and well-being?");
+        prompts.Add("What is something you want to remember about this day?");
+
+        for(int i=0; i < prompts.Count();i++)
+        {
+            Console.WriteLine(prompts[i]);
+        }
     }
 }
