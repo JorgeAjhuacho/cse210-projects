3e03e8dbfcc8d51ec71bad3d72ec6b447f815829
jorgeajhuacho <ajh21001@byui.edu>
2023-01-23 00:55:16 -0400

    File completed

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+public class Scripture
+{
+    public  void Person()
+    {
+        Console.WriteLine("dsd");
+    }
+
+}
+
+public class Helloda
+{
+    public  void moduel()
+    {
+        Console.WriteLine("SDD");
+    }
+
+}
