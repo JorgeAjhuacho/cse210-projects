29ec88134fa370257774c497848d13b12f4906d7
jorgeajhuacho <ajh21001@byui.edu>
2023-01-23 02:03:34 -0400

    File completed. Final version

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,10 +10,13 @@ class Program
         string inputchapter = Console.ReadLine();
         Console.Write("Please enter the number of the first verse: ");
         string firstNumVerse = Console.ReadLine();
-        Console.Write("Please enter the number of the last verse: (If there is no more than 1 verse, write 'no'");
+        Console.Write("Please enter the number of the last verse: (If there is no more than 1 verse, write 'no'): ");
         string lastNumVerse = Console.ReadLine();
         Console.Write("Almost done!. \nPlease enter the scripture: ");
         string scripture = Console.ReadLine();
+        bool conditional = true;
+        List<string> list =new();
+        list = scripture.Split(" ").ToList();
 
         //New Objects:
         Scripture BaseScripture = new(scripture);
@@ -21,9 +24,20 @@ class Program
         Reference reference = new(inputbook,inputchapter,firstNumVerse,lastNumVerse);
         string baseReference = reference.GetCompleteReference();
 
-        Console.WriteLine(baseScrip);
-        Console.WriteLine(baseReference);
+        Console.Clear();
+        Console.WriteLine($"{baseReference}{baseScrip}");
+        TextConverter converter = new();
+        converter.GetAllPhrase(baseReference,baseScrip,conditional,list);
 
+         List<string> listEdited = new();
+
+        listEdited.Add(reference + " ");
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            listEdited.Add((string)list[i] + " ");
+            Console.Clear();
+        }
     }
 
 
