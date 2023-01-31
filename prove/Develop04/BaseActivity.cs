acb72d2f0d1242bf0835c306e0f7e4de63c913f2
jorgeajhuacho <ajh21001@byui.edu>
2023-01-31 10:41:53 -0400

    Father file completed

---
+++ prove/Develop04/BaseActivity.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+
+public class BaseActivity
+{
+    private string _activityName;
+    private int _activityDuration;
+    private string _activityDescription;
+    private string _finalMessage;
+
+    public BaseActivity()
+    {
+        //Constructot for default values
+        _activityName = "Unknown";
+        _activityDescription = "Unknown";
+        _finalMessage = "Unknown";
+    }
+     public BaseActivity(string ActName)
+     {
+        _activityName = ActName;
+        _activityDescription = "Unknown";
+        _finalMessage = "Unknown";
+     }
+     public BaseActivity(string ActName, string ActivityDescription)
+     {
+        _activityName = ActName;
+        _activityDescription = ActivityDescription;
+     }
+    
+    public BaseActivity(string ActName, string ActivityDescription, string FinalMessage)
+    {
+        _activityName = ActName;
+        _activityDescription = ActivityDescription;
+        _finalMessage = FinalMessage;
+    }
+
+    //------Getter and Setters------
+    public string GetActName()
+    {
+        return _activityName;
+    }
+
+    public void SetActName(string ActName)
+    {
+        _activityName = ActName;
+    }
+
+    public string GetActDescription()
+    {
+        return _activityDescription;
+    }
+
+    public void SetActDescription(string ActivityDescription)
+    {
+        _activityDescription = ActivityDescription;
+    }
+    public string GetFinalMessage()
+    {
+        return _finalMessage;
+    }
+
+    public void SetFinalMessage(string FinalMessage)
+    {
+        _finalMessage = FinalMessage;
+    }
+
+    
+}
