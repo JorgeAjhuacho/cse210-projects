6a79cf0720f7c60fe0451fddf38285e7d4634f07
jorgeajhuacho <ajh21001@byui.edu>
2023-02-14 21:16:01 -0400

    Learning 05 Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square num1 = new Square("Red", 4);
+        shapes.Add(num1);
+        Rectangle num2 = new Rectangle("Red", 4, 5);
+        shapes.Add(num2);
+        Circle num3 = new Circle("Red", 7);
+        shapes.Add(num3);
+
+        foreach (Shape i in shapes)
+        {
+            string color = i.GetColor();
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
