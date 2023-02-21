dffa6332aef9ce7b2b16cfe5b469fd1fd3c44b3d
jorgeajhuacho <ajh21001@byui.edu>
2023-02-20 22:07:26 -0400

    Completed

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int userPoints = 0;
+        int option = 0;
+
+        bool completedSimpleGoal = false;
+        List<Goal> listOfGoals = new List<Goal>();
+
+        while (option != 6)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"You have {userPoints} points.");
+            Console.WriteLine();
+            Console.WriteLine("[1] Create a New Goal");
+            Console.WriteLine("[2] List Goals");
+            Console.WriteLine("[3] Save Goals");
+            Console.WriteLine("[4] Load Goals");
+            Console.WriteLine("[5] Record Event");
+            Console.WriteLine("[6] Quit");
+            Console.Write("Select a choice from the menu: ");
+            option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("The type of Goals are: ");
+                    Console.WriteLine("[1] Simple Goal");
+                    Console.WriteLine("[2] Eternal Goal");
+                    Console.WriteLine("[3] Checklist Goal");
+                    Console.WriteLine("Which type of goal would you like to create? ");
+                    int goalOption = int.Parse(Console.ReadLine());
+                    switch(goalOption)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.Write("What is the name of your goal? ");
+                            string simpleGoalName = Console.ReadLine();
+                            Console.Write("What is a short description of it? ");
+                            string simpleGoalDescription = Console.ReadLine();
+                            Console.Write("What is the amount associated to this goal? ");
+                            
+                    }
+            }
+        }
     }
 }
