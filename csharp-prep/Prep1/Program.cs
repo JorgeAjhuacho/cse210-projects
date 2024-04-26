88f849d651785edfd7582467d36a643fc646903e
[JorgeAjhuacho] <[ajh21001@byui.edu]>
2024-04-25 21:30:09 -0400

    Prep 1 Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your first lastname? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
+        
     }
 }
